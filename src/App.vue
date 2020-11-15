3380085b32e28e4c646c46b2d0394ee07cd12297
Sarah Manning <49659944+smanning29@users.noreply.github.com>
2020-11-15 16:47:51 -0700

    working three.js

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,51 @@
 <template>
-  <div id="app">
-    <canvas id="myCanvas"></canvas>
-  </div>
+    <div id="container"></div>
 </template>
 
 <script>
-import store from './store'
-import * as THREE from 'three';
-
+import * as Three from 'three'
 
 export default {
-  name: 'App',
-  components: {
+  name: 'app',
+  data() {
+    return {
+      camera: null,
+      scene: null,
+      renderer: null,
+      mesh: null
+    }
   },
-  store,
   methods: {
-    init: function(){
-      var container = document.getElementById('#myCanvas');
-      this.camera = new THREE.PerspectiveCamera(75, container.clientWidth/container.clientHeight, 1, 1000);
-      this.camera.position.z = 1;
-      this.scene = new THREE.Scene();
-      let geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
-      let material = new THREE.MeshNormalMaterial();
+    init: function() {
+        let container = document.getElementById('container');
+        console.log(container);
 
-        this.mesh = new THREE.Mesh(geometry, material);
+        this.camera = new Three.PerspectiveCamera(70, window.innerWidth/window.innerHeight, 0.01, 10);
+        this.camera.position.z = 1;
+
+        this.scene = new Three.Scene();
+
+        let geometry = new Three.BoxGeometry(0.2, 0.2, 0.2);
+        let material = new Three.MeshNormalMaterial();
+
+        this.mesh = new Three.Mesh(geometry, material);
         this.scene.add(this.mesh);
 
-        this.renderer = new THREE.WebGLRenderer({antialias: true});
-        this.renderer.setSize(container.clientWidth, container.clientHeight);
+        this.renderer = new Three.WebGLRenderer({antialias: true});
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
         container.appendChild(this.renderer.domElement);
 
-
+    },
+    animate: function() {
+        requestAnimationFrame(this.animate);
+        this.mesh.rotation.x += 0.01;
+        this.mesh.rotation.y += 0.02;
+        this.renderer.render(this.scene, this.camera);
     }
   },
-  computed: {
-    count () {
-      return this.$store.state.count
-    }
+  mounted() {
+      this.init();
+      this.animate();
   }
 }
 </script>
@@ -49,5 +58,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#container {
+  max-width: 960px;
 }
 </style>
