74a0a96242c9684e669c30029ce7a5a412d8c55e
Phu-bit <57963725+Phu-bit@users.noreply.github.com>
2020-12-01 15:34:41 -0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -14,60 +14,6 @@ export default {
   components: {
     MommaComponent
   }
-//   data() {
-//     return {
-//       camera: null,
-//       scene: null,
-//       renderer: null,
-//       mesh: null
-//     }
-//   },
-//   methods: {
-//     init: function() {
-//         let container = document.getElementById('container');
-//         console.log(container);
-
-//         this.camera = new Three.PerspectiveCamera(70, window.innerWidth/window.innerHeight, 1, 10000);
-//         this.camera.position.z = 5;
-
-//         //resize window
-//         window.addEventListener("resize", () => {
-//         this.renderer.setSize(window.innerWidth, window.innerHeight);
-//         this.camera.aspect = window.innerWidth / window.innerHeight;
-
-//         this.camera.updateProjectionMatrix();
-//       });
-
-//         this.scene = new Three.Scene();
-
-//         let geometry = new Three.BoxGeometry(0.2, 0.2, 0.2);
-//         let material = new Three.MeshNormalMaterial();
-
-//         this.mesh = new Three.Mesh(geometry, material);
-//         this.scene.add(this.mesh);
-
-//         this.renderer = new Three.WebGLRenderer({antialias: true});
-//         this.renderer.setSize(window.innerWidth, window.innerHeight);
-//         container.appendChild(this.renderer.domElement);
-
-//     },
-//     animate: function() {
-//         requestAnimationFrame(this.animate);
-//         this.mesh.rotation.x += 0.01;
-//         this.mesh.rotation.y += 0.02;
-//         this.renderer.render(this.scene, this.camera);
-//     },
-//     secondCube: function(size) {
-//       let geometry = new Three.BoxGeometry(size, size, size);
-//       const material = new Three.MeshNormalMaterial();
-//       this.mesh = new Three.Mesh(geometry, material);
-//       this.scene.add(this.mesh);
-//     }
-//   },
-//   mounted() {
-//       this.init();
-//       this.animate();
-//   }
  }
 </script>
 
